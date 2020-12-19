fd33f0424dac9c24aae41307d4cbdd61fc7262a2
ajsiasia <60340841+ajsiasia@users.noreply.github.com>
2020-12-19 21:58:46 +0800

    Add files via upload

---
+++ Activities/Endterm/Jstart/new.js
@@ -0,0 +1,219 @@
+var canvas = document.getElementById("art1");
+	var ctx = canvas.getContext("2d"); 
+	
+	
+	ctx.beginPath();
+	ctx.moveTo(100,0);
+	ctx.lineTo(100,75);
+	ctx.lineTo(160,100);
+	ctx.lineTo(100,100);
+	ctx.lineTo(100,100);
+	ctx.closePath();
+	ctx.fillStyle = "#fc5e49";
+	ctx.fill();
+ 
+    ctx.beginPath();
+	ctx.moveTo(100,100);
+	ctx.lineTo(100,80);
+	ctx.lineTo(100,10);
+	ctx.lineTo(90,80);
+	ctx.lineTo(40,40);
+	ctx.closePath();
+	ctx.fillStyle = "#fa9146";
+	ctx.fill();
+	
+   
+   var canvas = document.getElementById("art2");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.beginPath();
+	ctx.moveTo(107,37);
+	ctx.lineTo(157,117);
+	ctx.lineTo(107,117);
+	ctx.lineTo(107,37);
+	ctx.closePath();
+	ctx.fillStyle = "#0091AD";
+	ctx.fill();
+    
+    ctx.beginPath();
+	ctx.moveTo(101,37);
+	ctx.lineTo(51,117);
+	ctx.lineTo(101,117);
+	ctx.lineTo(101,37);
+	ctx.closePath();
+	ctx.fillStyle = "#1792ff";
+	ctx.fill();
+	
+	 var canvas = document.getElementById("art3");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.beginPath();
+	ctx.moveTo(73,190);
+	ctx.lineTo(138,130);
+	ctx.lineTo(73,69);
+	ctx.lineTo(73,107);
+	ctx.lineTo(5,107);
+	ctx.lineTo(5,152);
+	ctx.lineTo(73,152);
+	ctx.lineTo(73,190);
+	ctx.closePath();
+	ctx.fillStyle = "#680fdb";
+	ctx.fill();
+    
+
+	
+	var canvas = document.getElementById("art4");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.beginPath();
+	ctx.moveTo(139,9);
+	ctx.lineTo(179,32);
+	ctx.lineTo(100,32);
+	ctx.lineTo(20,32);
+	ctx.lineTo(60,95);
+	ctx.lineTo(100,159);
+	ctx.lineTo(139,95);
+	ctx.closePath();
+	ctx.fillStyle = "#ffc117";
+	ctx.fill();
+    
+    ctx.beginPath();
+	ctx.moveTo(138,131);
+	ctx.lineTo(176,72);
+	ctx.lineTo(100,72);
+	ctx.lineTo(24,72);
+	ctx.lineTo(62,131);
+	ctx.lineTo(100,191);
+	ctx.lineTo(138,131);
+	ctx.closePath();
+	ctx.fillStyle = "#e00d14";
+	ctx.fill();
+	
+	var canvas = document.getElementById("art5");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.beginPath();
+	ctx.moveTo(110,143);
+	ctx.lineTo(110,143);
+	ctx.lineTo(74,181);
+	ctx.lineTo(9,111);
+	ctx.lineTo(44,73);
+	ctx.lineTo(74,105);
+	ctx.lineTo(164,9);
+	ctx.lineTo(200,47);
+	ctx.lineTo(110,143);
+	ctx.closePath();
+	ctx.fillStyle = "#32ed93";
+	ctx.fill();
+    
+    ctx.beginPath();
+	ctx.moveTo(107,154);
+	ctx.lineTo(107,154);
+	ctx.lineTo(74,188);
+	ctx.lineTo(13,125);
+	ctx.lineTo(46,90);
+	ctx.lineTo(74,120);
+	ctx.lineTo(159,33);
+	ctx.lineTo(192,67);
+	ctx.lineTo(107,154);
+	ctx.closePath();
+	ctx.fillStyle = "#a9fc35";
+	ctx.fill();
+	
+	
+	var canvas = document.getElementById("art6");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.beginPath();
+	ctx.moveTo(108,151);
+	ctx.lineTo(192,23);
+	ctx.lineTo(192,151);
+	ctx.lineTo(108,151);
+	ctx.closePath();
+	ctx.fillStyle = "#d65f0f";
+	ctx.fill();
+    
+    ctx.beginPath();
+	ctx.moveTo(8,23);
+	ctx.lineTo(90,151);
+	ctx.lineTo(8,151);
+	ctx.lineTo(8,23);
+	ctx.closePath();
+	ctx.fillStyle = "#f58d47";
+	ctx.fill();
+	
+	
+	var canvas = document.getElementById("art7");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.beginPath();
+	ctx.moveTo(80,5);
+	ctx.lineTo(45,115);
+	ctx.lineTo(80,115);
+	ctx.lineTo(55,195);
+	ctx.lineTo(150,84);
+	ctx.lineTo(100,84);
+	ctx.lineTo(135,5);
+	ctx.lineTo(80,5);
+	ctx.closePath();
+	ctx.fillStyle = "#deda78";
+	ctx.fill();
+    
+    ctx.beginPath();
+	ctx.moveTo(86,11);
+	ctx.lineTo(53,120);
+	ctx.lineTo(86,120);
+	ctx.lineTo(62,198);
+	ctx.lineTo(151,89);
+	ctx.lineTo(104,89);
+	ctx.lineTo(137,11);
+	ctx.lineTo(86,11);
+	ctx.closePath();
+	ctx.fillStyle = "#faf22d";
+	ctx.fill();
+	
+	var canvas = document.getElementById("art8");
+	var ctx = canvas.getContext("2d");
+	
+	ctx.beginPath();
+	ctx.moveTo(139,96);
+	ctx.lineTo(99,161);
+	ctx.lineTo(62,95);
+	ctx.lineTo(26,29);
+	ctx.lineTo(103,29);
+	ctx.lineTo(180,30);
+	ctx.lineTo(139,96);
+	ctx.closePath();
+	ctx.fillStyle = "#ac05ff";
+	ctx.fill();
+    
+    ctx.beginPath();
+	ctx.moveTo(69,74);
+	ctx.lineTo(37,97);
+	ctx.lineTo(103,96);
+	ctx.lineTo(168,94);
+	ctx.lineTo(135,72);
+	ctx.lineTo(102,50);
+	ctx.lineTo(69,74);
+	ctx.closePath();
+	ctx.fillStyle = "#ed1fb6";
+	ctx.fill();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
